ad5cf77e6a8aca644ecc5b5842645440d627073a
BEAUTIFULTSC <31916969+BEAUTIFULTSC@users.noreply.github.com>
2017-12-29 13:12:54 +0800

    Delete index.js

--- 3显示隐藏二维码/index.js
+++
@@ -1,6 +0,0 @@
-function changeClass(element, searchVal, replaceVal){
-      var classArr = element.className.split(" ");
-      var index = classArr.indexOf(searchVal);
-      classArr[index] = replaceVal;
-      element.className = classArr.join(" ");
-}
